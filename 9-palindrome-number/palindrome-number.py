73ee39900d341e5a30affe5e49e3045c32a32cca
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-04-29 10:31:46 +0530

    Time: 13 ms (42.03%) | Memory: 12.4 MB (54.56%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        a=str(x)
+        left=0
+        right=len(a)-1
+        while left<=right:
+            if a[left]!=a[right]:
+                return False
+            left+=1
+            right-=1
+        else:
+            return True
+        
