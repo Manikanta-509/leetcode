b33b21db4370b0e1dd506d04d700c7ab12ab584e
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-05-10 22:21:12 +0530

    Time: 9 ms (23.31%) | Memory: 12.6 MB (6.18%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def firstPalindrome(self, words):
+        for word in words:
+            if word==word[::-1]:
+                return word
+        return ""
+
+
+        
