e85d1b038044f19e8662f229d91af32f520dec90
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-05-16 22:21:26 +0530

    Time: 5 ms (67.34%) | Memory: 13.5 MB (49.33%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        idx=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[idx]=nums[idx],nums[i]
+                idx+=1
+        return nums        
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
