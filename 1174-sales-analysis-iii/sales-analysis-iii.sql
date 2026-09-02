4cac75ceed9c10e81d329186c88c35d9f50e60df
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-09-02 20:19:06 +0530

    Time: 1643 ms (10.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,7 @@
+SELECT product.product_id, product.product_name
+FROM product
+JOIN sales
+    ON product.product_id = sales.product_id
+GROUP BY product.product_id, product.product_name
+HAVING MIN(sales.sale_date) >= '2019-01-01'
+   AND MAX(sales.sale_date) <= '2019-03-31';
