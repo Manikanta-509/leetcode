45f06e9d7fe6cd426c1f2d3477bd9e3ac6c1be3e
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-09-15 19:57:42 +0530

    Time: 461 ms (46.12%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,1 +1,2 @@
-select * from patients where (conditions like 'DIAB1%' or conditions like '% DIAB1%')
+/*select * from patients where (conditions like 'DIAB1%' or conditions like '% DIAB1%')*/
+select * from patients where (conditions like 'diab1%' or conditions like '% diab1%')
